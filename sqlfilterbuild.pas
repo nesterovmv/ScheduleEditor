be2def0659d8247eccc7b3bdac2d9ad441057683
nesterovmv <acht.rion@mail.ru>
2015-05-05 18:00:54 +1000

    Revert "Удалить неиспользуемый модуль"
    This reverts commit 4512bdcd7e0fb17d8338e902334c03a1580bdf38.

---
+++ sqlfilterbuild.pas
@@ -0,0 +1,87 @@
+unit SQLFilterBuild;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+  FilterPanel = class(TPanel)
+    CBoxOfFilterContact: TComboBox;
+    CBoxOfFields: TComboBox;
+    CBoxOfConditions: TComboBox;
+    EditConst: TEdit;
+    DeleteFilter: TButton;
+    constructor Create(TheOwner: TComponent);
+  end;
+
+implementation
+
+constructor FilterPanel.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+  Width := 900;
+  Height := 26;
+  Visible := True;
+  BevelOuter := bvNone;
+
+  CBoxOfFilterContact := TComboBox.Create(self);
+  with CBoxOfFilterContact do
+  begin
+   Parent := self;
+   Width := 60;
+   Height := 26;
+   Left := 24;
+   Visible := True;
+   ReadOnly := True;
+  end;
+
+  CBoxOfFields := TComboBox.Create(self);
+  with CBoxOfFields do
+  begin
+   Parent := self;
+   Width := 171;
+   Height := 26;
+   Left := 94;
+   Visible := True;
+   ReadOnly := True;
+  end;
+
+  CBoxOfConditions := TComboBox.Create(self);
+  with CBoxOfConditions do
+  begin
+   Parent := self;
+   Width := 171;
+   Height := 26;
+   Left := 275;
+   Visible := True;
+   ReadOnly := True;
+  end;
+
+  EditConst := TEdit.Create(self);
+  with EditConst do
+  begin
+   Parent := self;
+   Caption := '';
+   Width := 171;
+   Height := 26;
+   Left := 456;
+   Visible := True;
+  end;
+
+  DeleteFilter := TButton.Create(self);
+  with DeleteFilter do
+  begin
+   Parent := self;
+   Caption := 'удалить';
+   Width := 100;
+   Height := 26;
+   Left := 637;
+   Visible := true;
+  end;
+end;
+
+end.
